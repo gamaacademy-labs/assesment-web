cb53e3cf24ce3ad8d8db6811d400a68ed26d4d33
Andre Lima <andresdslima@gmail.com>
2022-08-13 20:40:43 +0200

    footer component waiting for programming logic

---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SFooterContainer = styled.div`
+  background-color: #7d38db;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100vw;
+  padding: 0.75rem 2rem;
+  margin: 0;
+  box-sizing: border-box;
+`;
