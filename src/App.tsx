3d732711720afaa573b67b510f369457eccd50b9
Junior Dering <juniordering@hotmail.com>
2022-08-13 09:59:37 -0400

    put defaultTheme

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { ThemeProvider } from "styled-components";
 import { ModalInfo } from "./components/ModalInfo";
 import { GlobalStyle } from "./styles/global";
 import { createTheme, SmashGlobalStyle } from "@gama-academy/smash-web";
+import { defaultTheme } from "./styles/themes/default";
 
 const theme = createTheme();
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       <ThemeProvider theme={theme}>
         <SmashGlobalStyle />
         <ModalInfo />
         <GlobalStyle />
       </ThemeProvider>
-    </div>
+    </ThemeProvider>
   );
 }
 
