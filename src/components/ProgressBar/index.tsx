38d78a995d06b7902d95458e6359be76d1bcd73b
Junior Dering <juniordering@hotmail.com>
2022-08-13 09:33:29 -0400

    improving responsiveness

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -6,44 +6,107 @@ import {
   Typography,
 } from "@gama-academy/smash-web";
 import { useState } from "react";
-import icon from "../../assets/icon.png";
-import { Div, DivButton, DivInformation } from "./styles";
+import {
+  Div,
+  DivAllButton,
+  DivButton,
+  DivInfoBar,
+  DivInformation,
+} from "./styles";
 
 export const ProgressBar = () => {
   const [num, setNum] = useState(0);
   let numQuestion = 4;
 
-  const nextQuestion = () => {
-    if (num < numQuestion) {
+  const nextQuestion = (signal: string) => {
+    if (signal == "+" && num < numQuestion) {
       setNum(num + 1);
+      console.log(num);
+    }
+    if (signal == "-" && num >= 1) {
+      setNum(num - 1);
       console.log(num);
     }
   };
 
   return (
     <Div>
-      <DivInformation>
-        <img src={icon} alt="icone" />
-        <p className="question">Questões</p>
-        <p className="numQuestion">
-          {num}/{numQuestion}
-        </p>
-      </DivInformation>
-      <Progress_Bar max={numQuestion} value={num} percentageSide="right" />
-      <DivButton>
-        <Button size="2" onClick={() => nextQuestion()} disabled={false}>
-          <Box alignment="center" dir="row" justifyContent="space-between">
-            <Typography fontWeight="semi_bold" mr="2" numberOfLines={1}>
-              {num < numQuestion ? "Prosseguir" : "Finalizar"}
-            </Typography>
-            <MaterialIcon
-              color="black"
-              name="arrow_forward"
-              shapeBackground="primary.3"
-            />
-          </Box>
-        </Button>
-      </DivButton>
+      <DivInfoBar>
+        <DivInformation>
+          <MaterialIcon color="white" name="ballot" />
+          <p className="question">Questões</p>
+          <p className="numQuestion">
+            {num}/{numQuestion}
+          </p>
+        </DivInformation>
+        <Progress_Bar
+          max={numQuestion}
+          value={num}
+          percentageSide="right"
+          margin={undefined}
+          m={undefined}
+          mt={undefined}
+          marginTop={undefined}
+          mb={undefined}
+          marginBottom={undefined}
+          ml={undefined}
+          marginLeft={undefined}
+          mr={undefined}
+          marginRight={undefined}
+          my={undefined}
+          marginY={undefined}
+          mx={undefined}
+          marginX={undefined}
+        />
+      </DivInfoBar>
+      <DivAllButton>
+        <DivButton num={num}>
+          <Button
+            className="backButton"
+            size="2"
+            fluid
+            onClick={() => nextQuestion("-")}
+            variant="filled"
+            color="brand.secondary"
+          >
+            <Box alignment="center" dir="row" justifyContent="space-between">
+              <MaterialIcon
+                color="black"
+                name="arrow_back"
+                shapeBackground="primary.3"
+              />
+              <Typography fontWeight="semi_bold" mr="2" numberOfLines={1}>
+                Anterior
+              </Typography>
+            </Box>
+          </Button>
+        </DivButton>
+        <DivButton>
+          <Button
+            size="2"
+            fluid
+            onClick={() => nextQuestion("+")}
+            disabled={false}
+          >
+            <Box
+              className="box"
+              alignment="center"
+              dir="row"
+              justifyContent="space-between"
+            >
+              <Typography fontWeight="semi_bold" mr="2" numberOfLines={1}>
+                {num < numQuestion ? "Próximo" : "finalizar"}
+              </Typography>
+              <MaterialIcon
+                color="black"
+                type="outlined"
+                name={num < numQuestion ? "arrow_forward" : "flag"}
+                shapeBackground="primary.3"
+              />
+            </Box>
+          </Button>
+        </DivButton>
+      </DivAllButton>
     </Div>
   );
 };
--- src/components/ProgressBar/styles.ts
+++ src/components/ProgressBar/styles.ts
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 interface props{
   margin?: number
+  num?: number;
 }
 
 export const Div = styled.div`
   position: absolute;
-  width: 78.37rem;
+  width: 100%;
   height: 4.5rem;
   left: 0px;
   bottom: 0px;
@@ -14,23 +15,66 @@ export const Div = styled.div`
   align-items: center;
   padding: 16px 32px;
   background: #7d38db;
-  color: white;
   box-shadow: 0px -2px 4px rgba(36, 37, 35, 0.24);
-  
+
+  @media (max-width: 768px) {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    height: auto;
+    gap: 1rem;
+  }
 `;
+
+export const DivInfoBar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-right: 2.5rem;
+
+  @media (max-width: 768px) {
+    margin-right: 0;
+  }
+`
+
 export const DivInformation = styled.div<props>`
   display: flex;
   align-items: center;
+  color: white;
 
   .question{
     display: flex;
     margin-left: 0.75rem;
+    @media (max-width: 768px) {
+      display: none;
+    }
   }
   .numQuestion{
     margin: 0 0.5rem;
   }
 `;
 
-export const DivButton = styled.div`
-  margin-left: 1rem;
+export const DivAllButton = styled.div`
+  display: flex;
+  gap: 1rem;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
+
+export const DivButton = styled.div<props>`
+  display: ${props=>props.num == 0 ? 'none' : 'block'};
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+  
+  .backButton{
+    border: 1px solid white;
+    color: white;
+    &:active{
+      border: none;
+    }
+  }
+
 `
