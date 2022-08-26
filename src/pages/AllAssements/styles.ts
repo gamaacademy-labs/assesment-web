1d8b8796c2024e1d104948ca5be2f55548eedc37
Junior Dering <juniordering@hotmail.com>
2022-08-26 09:10:31 -0400

    feat: all assessments page

---
+++ src/pages/AllAssements/styles.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100vw;
+	height: 100vh;
+`;
+export const Content = styled.div`
+	width: 80%;
+	height: 70%;
+	border: 1px solid #d7dbda;
+	border-radius: 8px;
+	margin: auto;
+	padding: 1rem;
+  position: relative;
+
+	.avaliation {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+		margin-top: 1rem;
+    padding: 1rem;
+		border: 1px solid #d7dbda;
+		border-radius: 8px;
+		width: 100%;
+    position: relative;
+
+    p{
+      margin: 0;
+    }
+	}
+
+  .teste{
+    border: 1px solid #d7dbda;
+		border-radius: 8px;
+    margin-bottom: 1rem;
+    display: flex;
+    position: absolute;
+    left: 0;
+  }
+`;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Login } from "./pages/Login";
 import { HomeAssessment } from "./pages/HomeAssessment";
 import { Assessment } from "./pages/Assessment";
 import Cookies from "js-cookie"
+import AllAssements from "./pages/AllAssements";
 
 export const MyRoutes = () => {
 	const token = Cookies.get("user");
@@ -15,6 +16,10 @@ export const MyRoutes = () => {
 				<Route
 					path="/assessment"
 					element={token ? <Assessment /> : <Login />}
+				/>
+				<Route
+					path="/allassessments"
+					element={token ? <AllAssements /> : <Login />}
 				/>
 			</Routes>
 		</Router>
