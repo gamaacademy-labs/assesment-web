299861d0492da00cbba1e7294b80ca3deab1a68b
Junior Dering <juniordering@hotmail.com>
2022-08-29 16:45:55 -0400

    feat: improve login

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import { GlobalStyle } from './styles/global';
 import { createTheme, SmashGlobalStyle } from '@gama-academy/smash-web';
 import { defaultTheme } from './styles/themes/default';
 import { MyRoutes } from './routes';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
 			<ThemeProvider theme={createTheme()}>
-				<SmashGlobalStyle />
-				<GlobalStyle />
-				<MyRoutes />
+				<Provider store={store}>
+					<SmashGlobalStyle />
+					<GlobalStyle />
+					<MyRoutes />
+				</Provider>
 			</ThemeProvider>
 		</ThemeProvider>
 	);
