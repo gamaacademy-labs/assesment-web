2546f85c9c74b393dcd66e2074de3f3196a9598e
Junior Dering <juniordering@hotmail.com>
2022-09-10 14:23:14 -0400

    feat: imporve code

--- src/components/ReviewQuestion/index.tsx
+++ src/components/ReviewQuestion/index.tsx
@@ -1,7 +1,7 @@
-import { Container, ContainerInput } from './styles';
+import { Container, ContainerInput, Content } from './styles';
 import iconAvaliationQuestions from '../../assets/icons/iconAvaliationQuestions.svg';
 import { useState } from 'react';
-import { Alternative } from '../../@types';
+import { Alternative, CorrectAnswer } from '../../@types';
 import { MaterialIcon } from '@gama-academy/smash-web';
 
 interface ReviewQuestionProps {
@@ -9,7 +9,7 @@ interface ReviewQuestionProps {
 	name: string;
 	title: string;
 	index: number;
-	correctAnswer: any;
+	correctAnswer: CorrectAnswer;
 }
 
 export function ReviewQuestion({
@@ -19,19 +19,21 @@ export function ReviewQuestion({
 	correctAnswer,
 }: ReviewQuestionProps) {
 	const [showQuestion, setShowQuestion] = useState(false);
+
 	const richtTextConverter = (richText: string) => {
 		return { __html: richText };
 	};
 
-	const checkedAnswer = (indexAlternative: number) => {
+	const checkedAnswerColor = (indexAlternative: number) => {
 		if (indexAlternative === correctAnswer.Correct[0]) {
 			return '#0FD03F';
 		}
 		if (indexAlternative === correctAnswer.alternativeId) {
-			return 'red';
+			return '#E2002F';
 		}
 		return '#fff';
 	};
+
 	console.log(correctAnswer);
 
 	return (
@@ -43,29 +45,59 @@ export function ReviewQuestion({
 						<MaterialIcon color="#202020" name="expand_more" />
 					</div>
 				</h3>
+
 				<div className="questionBody">
 					<div dangerouslySetInnerHTML={richtTextConverter(title)} />
+
 					{alternative.map((item, index) => (
-						<ContainerInput
-							key={item.id}
-							variant={checkedAnswer(index + 1)}
-							checked={checkedAnswer(index + 1) !== '#fff' ? true : false}
-						>
-							<input
-								onChange={() => {}}
-								value={item.id}
-								id={item.id}
-								type="radio"
-								disabled
-							/>
-							<label className="styledInput" htmlFor={item.id}>
-								<div className="active" />
-							</label>
-							<label htmlFor={item.id}>
-								{item.title} e {correctAnswer.Correct[0]} marcada
-								{correctAnswer.alternativeId}
-							</label>
-						</ContainerInput>
+						<Content>
+							<ContainerInput
+								variant={checkedAnswerColor(index + 1)}
+								changeDisplay={
+									checkedAnswerColor(index + 1) !== '#fff' ? true : false
+								}
+								width={10}
+								marginRight={1}
+							>
+								<strong>
+									{checkedAnswerColor(index + 1) === '#0FD03F' ? (
+										<span className="checked-answers">
+											<MaterialIcon color="##fff" size={13} name="done" />
+											Correta
+										</span>
+									) : (
+										<span className="checked-answers">
+											<MaterialIcon color="##fff" size={13} name="close" />
+											Escolhida
+										</span>
+									)}
+								</strong>
+							</ContainerInput>
+
+							<ContainerInput
+								width={100}
+								key={item.id}
+								variant={checkedAnswerColor(index + 1)}
+								checked={
+									checkedAnswerColor(index + 1) !== '#fff' ? true : false
+								}
+								changeDisplay
+							>
+								<input
+									onChange={() => {}}
+									value={item.id}
+									id={item.id}
+									type="radio"
+									disabled
+								/>
+
+								<label className="styledInput" htmlFor={item.id}>
+									<div className="active" />
+								</label>
+
+								<label htmlFor={item.id}>{item.title}</label>
+							</ContainerInput>
+						</Content>
 					))}
 				</div>
 			</Container>
--- src/components/ReviewQuestion/styles.ts
+++ src/components/ReviewQuestion/styles.ts
@@ -40,38 +40,41 @@ export const Container = styled.main<TypeContainer>`
 	}
 
 	p {
-		font-size: 0.75rem;
-		font-weight: 500;
-		margin-bottom: 1rem;
-		line-height: 1.2;
-	}
-
-	span {
-		font-size: 0.75rem;
-		margin-top: 0.5rem;
-		font-weight: 600;
+		margin: 0;
 	}
 `;
 
 interface TypeContainerInput {
-	variant: string;
-	checked: boolean;
+	variant?: string;
+	checked?: boolean;
+	width: number;
+	marginRight?: number;
+	changeDisplay?: boolean;
 }
 
 export const ContainerInput = styled.div<TypeContainerInput>`
+	display: ${props => (props.changeDisplay ? 'flex' : 'none')};
+	align-items: center;
 	font-size: 0.75rem;
 	font-weight: 700;
+	margin-bottom: 4px;
+	margin-right: ${props => props.marginRight}%;
+	margin-left: 4px;
 	margin-top: 1rem;
-	display: flex;
-
+	width: ${props => props.width}%;
 	border: 1px solid #ccd1cf;
 	border-radius: 8px;
-
 	background: ${props => props.variant};
 	color: ${props =>
-		props.variant === 'red' ? '#FFF' : props.theme['color-text']};
+		props.variant === '#E2002F' ? '#fff' : props.theme['color-text']};
 	box-shadow: ${props =>
 		props.variant !== '#fff' ? '-4px 4px 0px #202020' : '0'};
+
+	@media (max-width: 768px) {
+		width: ${props => (props.width === 100 ? props.width-1 : 4 * props.width)}%;
+		height: 30px;
+		font-size:1rem;
+	}
 
 	input {
 		display: none;
@@ -80,7 +83,6 @@ export const ContainerInput = styled.div<TypeContainerInput>`
 	label {
 		padding: 0.5rem 0;
 		width: 100%;
-		cursor: pointer;
 	}
 	.styledInput {
 		display: flex;
@@ -92,16 +94,35 @@ export const ContainerInput = styled.div<TypeContainerInput>`
 		padding: 0;
 		margin: auto 0.5rem;
 		border: ${props =>
-			props.variant === 'red' ? '1px solid #fff' : '1px solid black'};
+			props.variant === '#E2002F' ? '1px solid #fff' : '1px solid black'};
 		border-radius: 50%;
 
 		.active {
 			width: 4px;
 			height: 4px;
 			background-color: ${props =>
-				props.variant === 'red' ? '#fff' : 'black'};
+				props.variant === '#E2002F' ? '#fff' : 'black'};
 			border-radius: 50%;
-			display: ${props => props.checked ? 'block' : 'none'};
+			display: ${props => (props.checked ? 'block' : 'none')};
 		}
+	}
+
+	.checked-answers {
+		display: flex;
+		margin: 0;
+		align-items: center;
+		gap: 0.1rem;
+		margin-left: 0.5rem;
+		@media (max-width: 768px){
+			font-size: 1rem;
+
+		}
+	}
+`;
+
+export const Content = styled.div`
+	display: flex;
+	@media (max-width: 768px) {
+		flex-direction: column;
 	}
 `;
