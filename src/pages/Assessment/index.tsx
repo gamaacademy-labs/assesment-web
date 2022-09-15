5ff2f7f385179a6ee6d3851ea6de902f073c1aa8
Junior Dering <juniordering@hotmail.com>
2022-09-15 14:18:58 -0400

    fix: modal assessment in progress bar

--- src/pages/Assessment/index.tsx
+++ src/pages/Assessment/index.tsx
@@ -1,20 +1,26 @@
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { Question } from '../../@types';
 import { AvaliationQuestions } from '../../components/AvaliationQuestions';
 import { Header } from '../../components/Header';
+import { ModalAssessment } from '../../components/ModalAssessment';
 import { ModalInfo } from '../../components/ModalInfo';
 import { ProgressBar } from '../../components/ProgressBar';
 import { QuestionsMap } from '../../components/QuestionsMap';
 import { api } from '../../services/mainApi';
 import { getAssessmentQuestion } from '../../services/mainApi/assessments';
+import { finishingAssessment } from '../../services/userAssessment';
 import { RootState } from '../../store';
 import { setUser } from '../../store/user';
 import { Container, SubContainer } from './styles';
 
 export const Assessment = () => {
+	const navigate =useNavigate()
 	const [showModalInfo, setShowModalInfo] = useState(false);
+	const [showModalAssessment, setShowModalAssessment] = useState(false);
 	const [questions, setQuestions] = useState<Question[]>([]);
 	const [questionIndex, setQuestionIndex] = useState(0);
 	const [checkQuestionAnswer, setCheckQuestionAnswer] = useState<string[]>([]);
@@ -24,6 +30,12 @@ export const Assessment = () => {
 	const deadline = Cookies.get('dateAssessment') as string;
 	const dispatch = useDispatch();
 	const token = useSelector((state: RootState) => state.persistedReducer.token);
+
+	const sentAssessment = async () => {
+		await finishingAssessment(assessmentId)
+		navigate('/success')
+		return toast.success('Avaliação entregue!');
+	};
 
 	useEffect(() => {
 		const getQuestionList = async () => {
@@ -64,13 +76,22 @@ export const Assessment = () => {
 				questionIndex={questionIndex}
 				setQuestionIndex={setQuestionIndex}
 				checkQuestionAnswer={checkQuestionAnswer}
+				setShowModal={setShowModalAssessment}
 			/>
 			<ModalInfo
 				deadline={deadline}
 				showModal={showModalInfo}
 				setShowModal={setShowModalInfo}
 			/>
-
+			<ModalAssessment
+				title="Tem certeza que deseja entregar sua avaliação?"
+				text="Uma vez entregue, você não poderá alterar as respostas."
+				colorButton="success"
+				show={showModalAssessment}
+				textButton="Entregar avaliação"
+				onClickCancel={() => setShowModalAssessment(false)}
+				onClickConfirm={sentAssessment}
+			/>
 		</Container>
 	);
 };
